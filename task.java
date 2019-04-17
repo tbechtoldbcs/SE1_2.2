01cf6d9f4192fd9cf894c2273fea9ab31971826b
tbechtoldbcs <tobias.bechtold@smail.inf.h-brs.de>
2019-04-17 20:38:28 +0200

    init

--- task.java
+++ task.java
@@ -1,2 +1,12 @@
-class task {
+public class task {
+
+    String[][] s_Ausgang;
+
+    public task(String[][] s_Ausgang) {
+        this.s_Ausgang = s_Ausgang;
+    }
+
+    public boolean isWellSorted(String[] sequence) {
+        return false;
+    }
 }
